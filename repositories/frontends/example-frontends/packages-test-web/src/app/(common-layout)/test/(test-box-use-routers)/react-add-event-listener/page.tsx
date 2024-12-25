7519c00757c522b7c768ab31d85f4f2e7a7612e2
wisdomstar94 <wisdomstar94@gmail.com>
2024-12-25 21:40:02 +0900

    useAddEventListener 테스트

--- repositories/frontends/example-frontends/packages-test-web/src/app/(common-layout)/test/(test-box-use-routers)/react-add-event-listener/page.tsx
+++ repositories/frontends/example-frontends/packages-test-web/src/app/(common-layout)/test/(test-box-use-routers)/react-add-event-listener/page.tsx
@@ -1,11 +1,13 @@
-"use client"
+'use client';
 
 // import "@wisdomstar94/react-add-event-listener/style.css";
-import { TestBox, useAddEventListener, getVersion } from "@wisdomstar94/react-add-event-listener";
-import { useRouter } from "next/navigation";
+import { TestBox, useAddEventListener, getVersion } from '@wisdomstar94/react-add-event-listener';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Page() {
   const router = useRouter();
+  const [number, setNumber] = useState(0);
 
   useAddEventListener({
     windowEventRequiredInfo: {
@@ -13,24 +15,35 @@ export default function Page() {
       eventListener(event) {
         console.log('@window.click', event);
       },
+    },
+  });
+
+  useEffect(() => {
+    // getEventListeners 은 정식 api 가 아님.
+    // 코드 상에선 호출 할 수 없고, chrome dev tools 콘솔 창에서는 호출 가능함.
+    // 아래 코드는 호출 예시 목적으로 작성함.
+    const getEventListeners = (window as any).getEventListeners;
+    if (typeof getEventListeners === 'function') {
+      console.log(`getEventListeners(window)`, getEventListeners(window));
     }
-  })
+  }, [number]);
 
   return (
     <>
-      test/basic!  
+      test/basic!
       <TestBox />
+      <div>{getVersion()}</div>
       <div>
-        { getVersion() }
-      </div>
-      <div>
-        <button 
+        <button
           onClick={() => {
-            router.back();
-          }}>
-          뒤로가기
+            // router.back();
+            setNumber((prev) => prev + 1);
+          }}
+        >
+          1 증가
         </button>
       </div>
+      <div>({number})</div>
     </>
   );
 }
