52aa26036633f16d6d47df3da269b0c8402b4c98
wisdomstar94 <wisdomstar94@gmail.com>
2024-12-31 21:43:13 +0900

    custom height base responsive 예시 코드 작성.

--- repositories/frontends/example-frontends/tailwindcss-v4-example/src/app/page.tsx
+++ repositories/frontends/example-frontends/tailwindcss-v4-example/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="h4 flex flex-col gap-2">
+    <main className="h4 flex flex-col gap-2 h-sm:bg-amber-400">
       <div className="my-active:bg-red-500">hi</div>
       <button className="my-btn">my btn</button>
     </main>
