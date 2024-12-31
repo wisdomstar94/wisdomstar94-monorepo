df176da1690985579522ca46fd2331b42e881a6c
wisdomstar94 <wisdomstar94@gmail.com>
2024-12-31 21:49:32 +0900

    css 에 정의한 변수를 tailwind class 에서 사용하는 예시 코드 작성.

--- repositories/frontends/example-frontends/tailwindcss-v4-example/src/app/page.tsx
+++ repositories/frontends/example-frontends/tailwindcss-v4-example/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="h4 flex flex-col gap-2 h-sm:bg-amber-400">
       <div className="my-active:bg-red-500">hi</div>
       <button className="my-btn">my btn</button>
+      <div className="bg-(--my-color-a)">my color</div>
     </main>
   );
 }
