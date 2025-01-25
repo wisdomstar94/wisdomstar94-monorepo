fc6a5a5dd8b21effe5ebcca825f3adc42e4ce382
wisdomstar94 <wisdomstar94@gmail.com>
2025-01-25 12:30:30 +0900

    @wisdomstar94/xlsx-utils 패키지 작성 보류

--- repositories/packages/other-packages/xlsx-utils/src/functions/generate-excel/generate-excel.function.ts
+++ repositories/packages/other-packages/xlsx-utils/src/functions/generate-excel/generate-excel.function.ts
@@ -1,5 +1,17 @@
 import { IGenerageExcel } from './generate-excel.interface';
+import XLSX from 'xlsx';
 
 export function generateExcel(props: IGenerageExcel.Props) {
   console.log('hi!!!');
+
+  const rows = [
+    { name: 'aaa', birthday: '2000-02-02' },
+    { name: 'bbb', birthday: '1988-10-10' },
+    // ... one row per President
+  ];
+  const worksheet = XLSX.utils.json_to_sheet(rows);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Dates');
+  workbook.Sheets['Dates'].A1.s = { font: { bold: true } };
+  // XLSX.writeFile(workbook, 'Presidents.xlsx', { compression: true });
 }
