2e177dedfb8fca0ef15b981f5e154e65c3f37673
wisdomstar94 <wisdomstar94@gmail.com>
2025-05-23 00:27:53 +0900

    fix

--- repositories/packages/react-packages/react-number-flow/src/components/number-flow/number-flow.component.tsx
+++ repositories/packages/react-packages/react-number-flow/src/components/number-flow/number-flow.component.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import type { INumberFlow } from './number-flow.interface';
 import styles from './number-flow.module.css';
 
 export function NumberFlow(props: INumberFlow.Props) {
   const { value } = props;
+  const [timestamp, setTimestamp] = useState(0);
 
   const numberStringArr = useMemo(() => {
     const str = value.toString();
@@ -19,20 +20,29 @@ export function NumberFlow(props: INumberFlow.Props) {
     return num - 1;
   }
 
+  useEffect(() => {
+    setTimestamp(Date.now());
+  }, [value]);
+
   return (
     <>
-      <div className={styles['container']} data-active={true}>
+      <div
+        className={styles['container']}
+        data-active={true}
+        style={{ visibility: timestamp === 0 ? 'hidden' : undefined }}
+      >
         {numberStringArr.map((numStr, index) => {
           const num = Number(numStr);
           const num_1 = getMinusOneNum(num);
           const num_2 = getMinusOneNum(num_1);
           const num_3 = getMinusOneNum(num_2);
+          const num_4 = getMinusOneNum(num_3);
 
-          const transitionDelay = `${index * 0.04}s`;
+          const transitionDelay = `${index * 0.05}s`;
 
           return (
             <span
-              key={`${numStr}_${index}`}
+              key={`${numStr}_${index}_${timestamp}`}
               className={`${styles['num-unit-container']}`}
               style={{ transitionDelay: transitionDelay }}
             >
@@ -40,6 +50,7 @@ export function NumberFlow(props: INumberFlow.Props) {
               <span style={{ transitionDelay: transitionDelay }}>{num_1}</span>
               <span style={{ transitionDelay: transitionDelay }}>{num_2}</span>
               <span style={{ transitionDelay: transitionDelay }}>{num_3}</span>
+              <span style={{ transitionDelay: transitionDelay }}>{num_4}</span>
             </span>
           );
         })}
